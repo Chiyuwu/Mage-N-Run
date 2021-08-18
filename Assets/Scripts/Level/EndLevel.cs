bdb6b8e3468829858f4c77e861771d5116c2af1c
Leon Portius <Pluto-is-not-too-small@gmx.de>
2021-08-19 01:32:39 +0200

    inbetween idk

--- Assets/Scripts/Level/EndLevel.cs
+++ Assets/Scripts/Level/EndLevel.cs
@@ -6,18 +6,20 @@ public class EndLevel : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("Collectable"))
-        {
-            if (collision.gameObject.name.Equals("CollectableOfAllCollectables")) {
-                // stop wall & player movement
-                // stop score + time
-                // dont destroy on load game manager
-                // is current scene level 2?
-                // get player keyboard name
-                // serialize score, name, time
-                // make menu so data gets from save file
-            }
+        if (collision.gameObject.name.Equals("CollectableOfAllCollectables")) {
+            // stop wall & player movement
+            // stop score + time
+            // dont destroy on load game manager - check
+            // in levelmanagement set everything to 'go' again
+            
+            /// get player data
+            // is current scene level 2?
+            // get player keyboard name
+            
+            // serialize score, name, time
+            // make menu so data gets from save file
         }
+        
     }
 
 
